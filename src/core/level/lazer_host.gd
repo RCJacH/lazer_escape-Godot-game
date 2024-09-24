d09dda631ca21ca73ad449f6e7f4705dcd5c5208
RCJacH <RCJacH@outlook.com>
2024-09-25 03:05:10 +0800

    add LazerHost to Level

---
+++ src/core/level/lazer_host.gd
@@ -0,0 +1,14 @@
+extends Node2D
+class_name LazerHost
+
+
+func _on_control_parser_mouse_moved(position: Vector2, shift: bool, dragging: bool, press_position: Vector2) -> void:
+	pass
+
+
+func _on_control_parser_wheel_down_pressed(ctrl, alt, shift):
+	pass # Replace with function body.
+
+
+func _on_control_parser_wheel_up_pressed(ctrl, alt, shift):
+	pass # Replace with function body.
