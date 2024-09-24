f60669d58d32adec2d1c1545d4ad22a0c2075b45
RCJacH <RCJacH@outlook.com>
2024-09-25 01:12:10 +0800

    refactor LevelBoundaries to allow customization

--- src/core/level/boundary_area_2d.gd
+++ src/core/level/boundary_area_2d.gd
@@ -1,23 +1,42 @@
+@tool
 extends Area2D
 class_name LevelBoundaries
 
-@onready var collision_shape: CollisionPolygon2D = $CollisionPolygon2D
+@export var shrink_inner: float = 10.0 :
+	set(new_value):
+		shrink_inner = new_value
+@export var grow_outer: float = 10.0:
+	set(new_value):
+		grow_outer = new_value
+@export var update_now: bool = false:
+	set(new_value):
+		update_boundaries()
 
 
 func _ready() -> void:
-	var viewport := get_viewport_rect()
-	var margin := Vector2(10.0, 10.0)
-	var points: Array[Vector2] = [
-		viewport.position,
-		viewport.position + Vector2(viewport.size.x, 0.0),
-		viewport.end,
-		viewport.position + Vector2(0.0, viewport.size.y),
-		viewport.position - Vector2(0.0, 0.0),
-		viewport.position - margin,
-		viewport.position + Vector2(-margin.x, viewport.size.y + margin.y),
-		viewport.end + margin,
-		viewport.position + Vector2(viewport.size.x + margin.x, -margin.y),
-		viewport.position - Vector2(0.0, margin.y),
-		viewport.position,
+	update_boundaries()
+
+
+func update_boundaries(rect: Rect2 = Rect2()) -> void:
+	if not rect:
+		rect = get_viewport_rect()
+	var points: Array[Vector2] = _get_corners(rect.grow(-shrink_inner))
+	var outer_corner := _get_corners(rect.grow(grow_outer))
+	outer_corner.reverse()
+	points.append_array(outer_corner)
+	points.append(points[0])
+	update_polygon.bind(points).call_deferred()
+
+
+func update_polygon(points: Array[Vector2]) -> void:
+	$CollisionPolygon2D.polygon = PackedVector2Array(points)
+
+
+func _get_corners(rect: Rect2) -> Array[Vector2]:
+	return [
+		rect.position,
+		rect.position + Vector2(rect.size.x, 0.0),
+		rect.end,
+		rect.position + Vector2(0.0, rect.size.y),
+		rect.position,
 	]
-	collision_shape.polygon = PackedVector2Array(points)
