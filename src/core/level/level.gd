da2ff906abafcd6f5ac3db67da33be529a3f2f2f
RCJacH <RCJacH@outlook.com>
2024-10-03 06:24:09 +0800

    add passing win screen button signals upwards

--- src/core/level/level.gd
+++ src/core/level/level.gd
@@ -1,6 +1,13 @@
 @tool
 extends Node2D
 
+signal next_level()
+signal return_to_menu()
 
-func _on_level_boundaries_all_unlocked():
-	print("YOU WIN")
+
+func _on_level_ui_next_level():
+	next_level.emit()
+
+
+func _on_level_ui_return_to_menu():
+	return_to_menu.emit()
