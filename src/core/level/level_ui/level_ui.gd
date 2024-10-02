d98e341a24615a2f1dcd3379ec0bca324fef6ad9
RCJacH <RCJacH@outlook.com>
2024-10-03 06:20:40 +0800

    connect all boundaries unlocked to display win screen

--- src/core/level/level_ui/level_ui.gd
+++ src/core/level/level_ui/level_ui.gd
@@ -8,8 +8,15 @@ var has_next_level: bool = true
 
 
 func _ready() -> void:
+	visible = false
+	%WinScreen.visible = false
 	if not has_next_level:
 		%NextLevel.visible = false
+
+
+func display_win_screen() -> void:
+	visible = true
+	%WinScreen.visible = true
 
 
 func _on_menu_button_pressed():
