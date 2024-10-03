e9d67dd4bd16ddc392aced6f965440d08e154c11
RCJacH <RCJacH@outlook.com>
2024-10-03 08:34:36 +0800

    add memories to LevelDesignHelper

--- src/core/level/level_design_helper.gd
+++ src/core/level/level_design_helper.gd
@@ -4,7 +4,34 @@ class_name LevelDesignHelper
 
 signal position_changed(new_position: Vector2, relative: Vector2)
 
+
+@export var select_memory: int :
+	set(new_i):
+		if new_i >= memories.size():
+			new_i %= memories.size()
+		elif new_i < -1:
+			new_i += memories.size() + 1
+		select_memory = new_i
+		if new_i == -1:
+			return
+		position = memories[new_i]
+@export var total_memories: int:
+	set(i):
+		return
+	get():
+		return memories.size()
+@export var save_memory: bool :
+	set(v):
+		if select_memory == -1:
+			memories.append(position)
+			return
+		memories[select_memory] = position
+@export var delete_memory: bool:
+	set(v):
+		memories.remove_at(select_memory)
+
 var previous_position := Vector2.ZERO
+var memories: Array[Vector2] = []
 
 
 func _ready() -> void:
