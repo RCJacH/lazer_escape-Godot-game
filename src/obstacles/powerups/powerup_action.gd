4a28744dbbeeb3e5e22cbba9a86f8da8689510c1
RCJacH <RCJacH@outlook.com>
2024-10-04 03:57:43 +0800

    add signals to powerup action for editor use

--- src/obstacles/powerups/powerup_action.gd
+++ src/obstacles/powerups/powerup_action.gd
@@ -1,13 +1,30 @@
+@tool
 extends Resource
 class_name PowerUpAction
 
-@export_enum("+1:1", "-1:-1") var modifier := 1
-@export var mod_target: Target = Target.BOUNCE
+signal modifier_changed(value: int)
+signal target_changed(value: Target)
+
+@export_enum("+1:1", "-1:-1") var modifier := 1 :
+	set(new_modifier):
+		if new_modifier == modifier:
+			return
+
+		modifier = new_modifier
+		modifier_changed.emit(modifier)
+@export var mod_target: Target = Target.BOUNCE :
+	set(new_target):
+		if new_target == mod_target:
+			return
+
+		mod_target = new_target
+		target_changed.emit(mod_target)
 
 enum Target {
 	BOUNCE,
 	POWER,
 }
+
 
 func do(lazer: Lazer) -> void:
 	match mod_target:
